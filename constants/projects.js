08a399fa43697deb4d2e5827739f8e9c44ed2571
Alen Duda <alenduda@gmail.com>
2021-02-24 22:37:12 +0100

    Initial commit

---
+++ constants/projects.js
@@ -0,0 +1,8 @@
+module.exports = [
+    "accessible-radio-player",
+    "army-battle-sim",
+    "crossword",
+    "idle-strategy-backend",
+    "uniline-react",
+    "SimpleContacts"
+]
---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const projects = require('./constants/projects');
+
+const basePath = path.resolve(__dirname, '..');
+const packageFileName = 'package.json';
+
+const dependencyAggregate = [];
+
+projects.forEach(projectName => {
+    const projectDirPath = path.join(basePath, projectName);
+    const targetPackageFileName = path.join(projectDirPath, packageFileName);
+
+    if (!fs.existsSync(targetPackageFileName)) {
+        // package.json not found in target folder
+        return;
+    } else {
+        // parse package.json file and extract dependencies
+        const package = fs.readFileSync(targetPackageFileName, 'utf-8');
+        const parsedPackage = JSON.parse(package);
+
+        const dependencies = Object.keys(parsedPackage.dependencies);
+        dependencyAggregate.push(...dependencies);
+
+        // not all projects must have devDependencies (sure)
+        if (parsedPackage.devDependencies) {
+            const devDependencies = Object.keys(parsedPackage.devDependencies);
+            dependencyAggregate.push(...devDependencies);
+        }
+    }
+});
+
+dependencyAggregate.sort((a, b) => {
+    if (a > b) return 1;
+    else return -1;
+});
+
+// write wordlist to file
+const data = dependencyAggregate.join('\n');
+fs.writeFileSync('wordlist.txt', data, 'utf-8');
+
+// todo: use https://monkeylearn.com/word-cloud/ to generate a word cloud
